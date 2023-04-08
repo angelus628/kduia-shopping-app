8b260a55c98c5a6f2dfa5abb4b25d909581fdbfb
angelus628 <angelus628@hotmail.com>
2023-04-08 03:43:37 -0400

    inital commit

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,16 @@
 import React, { createContext, useReducer } from 'react';
+
+//5. The reducer - this is used to update the state, based on the action
+export const AppReducer = (state, action) => {
+    let new_expenses = [];
+    switch (action.type) {
+        case 'ADD_QUANTITY':
+            let updatedqty = false;
+            state.expenses.map((expense) => {
+                if (expense.name === action.payload.name) {
+                    expense.quantity = expense.quantity + action.payload.queatity;
+                    updatedqty = true;
+                }
+            });
+    }
+};
